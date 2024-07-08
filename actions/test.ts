09c5722d33a6a8574dbdfa8f9d755c6a55dde44a
wrekcaparamarthika <sttwrekcaparamarthika@gmail.com>
2024-07-08 23:07:15 +0800

    fix bug

---
+++ actions/test.ts
@@ -0,0 +1,8 @@
+// 'use server';
+// import { LoginSchema } from '@/schemas';
+// import * as z from 'zod';
+
+// export async function register(values: z.infer<typeof LoginSchema>) {
+// 	const validatedFields = LoginSchema.safeParse(values);
+// 	const { async } = validatedFields.data;
+// }
--- actions/w.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-import { LoginSchema } from '@/schemas';
-import * as z from 'zod';
-
-export async function register(values: z.infer<typeof LoginSchema>) {
-	const validatedFields = LoginSchema.safeParse(values);
-	const { async } = validatedFields.data;
-}
